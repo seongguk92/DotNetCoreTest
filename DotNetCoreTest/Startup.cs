b0f00b29249959c9a47236c9c6f7a8d597403e93
김성국 <live8681@naver.com>
2021-08-24 10:58:42 +0900

    카카오 인증 테스트

--- DotNetCoreTest/Startup.cs
+++ DotNetCoreTest/Startup.cs
@@ -1,27 +1,40 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DotNetCoreTest
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostEnvironment hostingEnvironment)
         {
             Configuration = configuration;
+            HostingEnvironment = hostingEnvironment;
         }
 
         public IConfiguration Configuration { get; }
 
+        private IHostEnvironment HostingEnvironment { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+
+           .AddKakaoTalk(options =>
+            {
+                options.ClientId = Configuration["KakaoTalk:ClientId"];
+                options.ClientSecret = Configuration["KakaoTalk:ClientSecret"];
+            });
+
             services.AddControllersWithViews();
         }
 
