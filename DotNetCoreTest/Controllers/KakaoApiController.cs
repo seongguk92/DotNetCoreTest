c59bb60f30ab560a7f34a6f844e3c21d02c8eb5d
seongguk <live8681@naver.com>
2021-09-03 00:39:35 +0900

    카카오 맵 띄어보기

---
+++ DotNetCoreTest/Controllers/KakaoApiController.cs
@@ -0,0 +1,32 @@
+﻿using DotNetCoreTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreTest.Controllers
+{
+    public class KakaoApiController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public KakaoApiController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult KakaoMap()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
